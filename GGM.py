07994194240b51108fc9daf6ba0dacf3c4b37593
mytruth126 <57935002+mytruth126@users.noreply.github.com>
2024-01-05 21:00:48 +0800

    Add files via upload

---
+++ GGM.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  4 16:59:20 2024
+
+@author: Dr. Lianyi Liu  , email (mytruth@126.com; liu1996@nuaa.edu.cn)
+"""
+
+import pandas as pd
+import numpy as np
+from gplearn.genetic import SymbolicRegressor
+from User_defined_Functions import first_accumulation, _equation_str, _print_error,replace_string
+from scipy.integrate import odeint
+import graphviz
+import sympy as sp
+
+class Generalized_grey_model(object):
+    def __init__(self,df,nf):
+        self.df=df
+        self.nf=nf
+    def generate_dataset(self):
+        [t,y]=[self.df['time'],self.df['data']]
+        self.t=np.array(t)
+        self.y=np.array(y)
+        self.n=y.shape[0]
+        self.x_1, self.t=first_accumulation(self.y.reshape(-1), self.t.reshape(-1), self.n)
+        datax=np.array(list(zip(self.t,self.x_1)))
+        self.train_size = int(len(datax) -self.nf)
+        self.test_size = len(datax)-self.train_size
+        self.x_train = datax[0:self.train_size]
+        self.y_train = self.y[0:self.train_size]
+        self.x_test = datax[self.train_size:]
+        self.y_test = self.y[self.train_size:]
+    def structure_identification_SG(self,function_set= ['add', 'sub', 'mul', 'div'],parsimony_coefficient=0.01):
+        self.generate_dataset()
+        self.est_gp = SymbolicRegressor(population_size=2000,
+                                   generations=20, stopping_criteria=0.01,
+                                   function_set=function_set,
+                                   const_range=(-1000,1000),
+                                   feature_names=['t','x_1'],
+                                   p_crossover=0.7, p_subtree_mutation=0.1,
+                                   p_hoist_mutation=0.05, p_point_mutation=0.1,
+                                   max_samples=0.9, verbose=1,
+                                   parsimony_coefficient=parsimony_coefficient, random_state=0)
+        self.est_gp.fit(self.x_train, self.y_train)
+        self.train_fit = self.est_gp.predict(self.x_train)
+        self.test_fit = self.est_gp.predict(self.x_test)
+        self.y_pre=np.concatenate((self.train_fit,self.test_fit))
+        #print(self.est_gp._program)
+        self.graphviz_pdf()
+        self.equation_str=_equation_str(self.est_gp._program)
+        self.print_equation()
+    def f_equation(self,x_1,t):
+            return eval(self.equation_str)
+    def predict(self,t):
+        # Define initial conditions
+        y0 = self.x_1[0]
+        # Using odeint functions to solve differential equations
+        x_1_pre = odeint(self.f_equation, y0, t)
+        self.y_pre2=np.concatenate((np.array([self.y[0]]),np.diff(x_1_pre.reshape(-1,))))
+        return self.y_pre2
+    def print_error(self):
+        _print_error(self.y,self.y_pre2,self.train_size)
+    def graphviz_pdf(self):
+        dot_data = self.est_gp._program.export_graphviz()
+        self.graph = graphviz.Source(dot_data)
+        #self.graph.view() #Output to PDF file
+    def print_equation(self):
+        t = sp.symbols('t')
+        x_1 = sp.Function('x^(1)')(t)
+        self.equation=sp.Eq(x_1.diff(t),sp.sympify(replace_string(self.equation_str)))
+
+if __name__ == '__main__':
+    #generate model
+    nf=4 #Test set size
+    df=pd.read_excel('data.xlsx')
+    GGM=Generalized_grey_model(df,nf)
+    #training model
+    function_set = ['add', 'sub', 'mul', 'div','log', 'sin','cos'] 
+    parsimony_coefficient=0.07 #Complexity penalty coefficient
+    GGM.structure_identification_SG(function_set,parsimony_coefficient)
+    #predict
+    y_predict=GGM.predict(GGM.t)
+    #error
+    GGM.print_error()
+    #Tree Formula Chart
+    GGM.graph
+    #equation
+    GGM.equation
+    sp.pprint(GGM.equation)
+
+
---
+++ User_defined_Functions.py
@@ -0,0 +1,124 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 17 19:36:16 2023
+
+@author: Elijah
+"""
+from gplearn.functions import _Function
+from sklearn.metrics import mean_absolute_percentage_error, mean_squared_error, mean_absolute_error
+import numpy as np
+
+def first_accumulation(y, t, n):
+    h = np.diff(t).astype(np.float64)
+    h=np.divide(h,np.mean(h))
+    t=np.cumsum(np.concatenate((np.array([1]),h)))
+    x_1=np.cumsum(np.concatenate((np.array([1]),h))*y)
+    return x_1,t
+
+def _equation_str(tmp):
+    """Overloads `print` output of the object to resemble a LISP tree."""
+    terminals = [0]
+    output = ''
+    for i, node in enumerate(tmp.program):
+        if isinstance(node, _Function):
+            terminals.append(node.arity)
+            if node.name=='add':
+                name='np.add'
+            if node.name=='sub':
+                name='np.subtract'
+            if node.name=='mul':
+                name='np.multiply'
+            if node.name=='div':
+                name='np.divide'
+            if node.name=='sqrt':
+                name='np.sqrt'
+            if node.name=='log':
+                name='np.log'
+            if node.name=='neg':
+                name='np.negative'
+            if node.name=='abs':
+                name='np.abs'
+            if node.name=='max':
+                name='np.maximum'
+            if node.name=='min':
+                name='np.minimum'
+            if node.name=='sin':
+                name='np.sin'
+            if node.name=='cos':
+                name='np.cos'
+            if node.name=='tan':
+                name='np.tan'
+            output += name + '('
+        else:
+            if isinstance(node, int):
+                if tmp.feature_names is None:
+                    output += 'X%s' % node
+                else:
+                    output += tmp.feature_names[node]
+            else:
+                output += '%.3f' % node
+            terminals[-1] -= 1
+            while terminals[-1] == 0:
+                terminals.pop()
+                terminals[-1] -= 1
+                output += ')'
+            if i != len(tmp.program) - 1:
+                output += ', '
+    return output
+
+def _print_error(y,y_pre,train_size):
+    print('The fitting MAE',
+          mean_absolute_error(y[0:train_size], y_pre[0:train_size]), '\n'
+          'The fitting MAPE',
+          mean_absolute_percentage_error(y[0:train_size], y_pre[0:train_size]),'\n'
+          'The forecasting MAE',
+          mean_absolute_error(y[train_size:], y_pre[train_size:]), '\n'
+          'The forecasting RMSE',
+          np.sqrt(mean_squared_error(y[train_size:], y_pre[train_size:])), '\n'
+          'The forecasting MAPE',
+          mean_absolute_percentage_error(y[train_size:], y_pre[train_size:])
+      )
+
+def NIP_accumulation(y, t, n, r):
+    NIP_matrix_r=np.zeros((n,n))
+    for i in range(n):
+        for j in range(i+1):
+            NIP_matrix_r[i][j]=np.power(r,t[i]-t[j])
+    xr=NIP_matrix_r@y
+    return xr,NIP_matrix_r
+
+def replace_function(substring,input_string):
+    if substring=='subtract':
+        replacement='-'
+    if substring=='add':
+        replacement='+'
+    if substring=='multiply':
+        replacement='*'
+    if substring=='divide':
+        replacement='/'
+    if substring in input_string:
+        position = input_string.find(substring)
+        tmp=0
+        ind=position-1
+        for i in input_string[position:]:
+            ind=ind+1 
+            if i=='(':
+                tmp=tmp+1
+            if i==')':
+                tmp=tmp-1
+            if i==',' and tmp==1:
+                output_string=input_string[:ind] + replacement + input_string[ind+1:]
+                break
+        output_string=output_string[:position]+output_string[position+len(substring):]
+    return output_string
+
+def replace_string(input_string):
+    input_string=input_string.replace('np.','')
+    replace_string = ['add','subtract','multiply','divide']
+    for substring in replace_string:
+        while substring in input_string:
+            input_string=replace_function(substring,input_string)
+    return input_string
+
+
+
